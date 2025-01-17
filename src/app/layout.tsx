30aece9c6be55e03918147528f5146e99bdfc90b
Lyle Liao <lyle@twjoin.com>
2025-01-17 11:18:18 +0800

    feat: remove local font (google font api only)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,13 @@
 import type { ReactNode } from 'react';
 
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
 
 import { ThemeProvider } from 'next-themes';
 
+import '@/app/fonts.css';
 import '@/app/globals.css';
 import NavigationBar from '@/components/share/NavigationBar';
 import { Toaster } from '@/components/ui/sonner';
-
-const geistSans = localFont({
-    src: './fonts/GeistVF.woff',
-    variable: '--font-geist-sans',
-    weight: '100 900'
-});
-const geistMono = localFont({
-    src: './fonts/GeistMonoVF.woff',
-    variable: '--font-geist-mono',
-    weight: '100 900'
-});
 
 export const metadata: Metadata = {
     title: 'Create Next App',
@@ -29,8 +18,8 @@ const Layout = ({ children }: Readonly<{ children: ReactNode }>) => {
     return (
         // ? https://github.com/pacocoursey/next-themes?tab=readme-ov-file#with-app
         // ? https://react.dev/reference/react-dom/client/hydrateRoot#suppressing-unavoidable-hydration-mismatch-errors
-        <html suppressHydrationWarning lang='zh-TW'>
-            <body className={`${geistSans.variable} ${geistMono.variable} bg-background text-foreground antialiased`}>
+        <html suppressHydrationWarning lang='zh-TW' className='scroll-smooth'>
+            <body className='antialiased'>
                 <ThemeProvider attribute='class'>
                     <NavigationBar />
                     {children}
